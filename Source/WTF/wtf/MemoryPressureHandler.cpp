5bbc961eac10cc040c397574554f3d9717219822
Marcin Mielczarczyk <marcin.mielczarczyk@redembedded.com>
2021-10-11 11:04:33 +0200

    [ONEM-21141] Enable memory periodic timer for WebProcess
    Enable periodic timer for memory pressure module for WPE WebProcess.
    Set timer repeat to 5 seconds.

--- Source/WTF/wtf/MemoryPressureHandler.cpp
+++ Source/WTF/wtf/MemoryPressureHandler.cpp
@@ -72,7 +72,7 @@ void MemoryPressureHandler::setShouldUsePeriodicMemoryMonitor(bool use)
 
     if (use) {
         m_measurementTimer = std::make_unique<RunLoop::Timer<MemoryPressureHandler>>(RunLoop::main(), this, &MemoryPressureHandler::measurementTimerFired);
-        m_measurementTimer->startRepeating(30_s);
+        m_measurementTimer->startRepeating(5_s);
     } else
         m_measurementTimer = nullptr;
 }
